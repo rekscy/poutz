9d3c4ffa6af1da14f871b5115119dbb823424793
shadoney <darkgangsta06@hotmail.com>
2012-11-11 22:52:00 +0100

    Taille de tableau staff

--- core/pages/staff.php
+++ core/pages/staff.php
@@ -28,8 +28,8 @@ if(!isset($_SESSION['name']) && !isset($_SESSION['accountLevel'])){
 ?>     
              <table class="voteTop" style="border:none">
                 <thead>
-                <th width='190'><strong>Nom</strong></th>
-                <th width='190'><strong>Grade</strong></th>
+                <th width='140'><strong>Nom</strong></th>
+                <th width='240'><strong>Grade</strong></th>
                 <th width='100'><strong>Depuis le:</strong></th>
                 <th width='60'><strong>Contacter</strong></th>
                 </thead>
